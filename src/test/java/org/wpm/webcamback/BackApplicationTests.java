35aae4713546a96ccd4fd563525adeef4abe9404
Willy PM <w.petitmaire@gmail.com>
2023-08-02 22:17:38 +0200

    build image docker & push

---
+++ src/test/java/org/wpm/webcamback/BackApplicationTests.java
@@ -0,0 +1,13 @@
+package org.wpm.webcamback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
