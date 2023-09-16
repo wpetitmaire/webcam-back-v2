ed6bd8744666f0f05cddffe28668d4fb5ffdcdb0
Willy PM <w.petitmaire@gmail.com>
2023-09-16 22:32:57 +0200

    run script

--- src/main/java/org/wpm/webcamback/helpers/CommandLineException.java
+++ src/main/java/org/wpm/webcamback/helpers/CommandLineException.java
@@ -2,10 +2,6 @@ package org.wpm.webcamback.helpers;
 
 public class CommandLineException extends Exception {
 
-    public CommandLineException(String message) {
-        super(message);
-    }
-
     public CommandLineException(String message, Throwable cause) {
         super(message, cause);
     }
